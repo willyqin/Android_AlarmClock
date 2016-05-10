755adcf8af114608d2f7b2685013308006884e96
qinwei <591356206@qq.com>
2016-05-10 23:39:56 +0800

    将RecycleView的数据清空后，提示目前没有闹钟；

--- app/src/main/java/com/example/han/myalarmclock/MainActivity.java
+++ app/src/main/java/com/example/han/myalarmclock/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.han.myalarmclock;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,9 +18,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
@@ -28,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     RecyclerView.Adapter mAdapter;
+    TextView textView;
     RecyclerView.LayoutManager layoutManager;
     List<Alarm> alarmList;
     /**
@@ -42,8 +54,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
+        textView = (TextView)findViewById(R.id.empty_image);
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,8 +63,10 @@ public class MainActivity extends AppCompatActivity {
                 Alarm newAlarm = new Alarm();
                 alarmList.add(newAlarm);
                 mAdapter.notifyDataSetChanged();
-                ColorDrawable drawable = new ColorDrawable(0x0099ee);
-                if (alarmList.size() == 1) recyclerView.setBackground(drawable);
+                if (alarmList.size() == 1){
+                    recyclerView.setVisibility(View.VISIBLE);
+                    textView.setVisibility(View.INVISIBLE);
+                }
 
             }
         });
@@ -62,10 +75,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        /*final Alarm[] alarms = new Alarm[6];
-        for(int i = 0; i < 6; i++ ){
-            alarms[i] = new Alarm();
-        }*/
         alarmList = new ArrayList<Alarm>();
         for (int i = 0; i < 6; i++){
             alarmList.add(new Alarm());
@@ -85,16 +94,18 @@ public class MainActivity extends AppCompatActivity {
                 final int pos = viewHolder.getAdapterPosition();
                 final Alarm tempAlarm = alarmList.remove(viewHolder.getAdapterPosition());
                 mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
-                final ColorDrawable drawable = new ColorDrawable(0x120099ee);
-                if(pos == 0) recyclerView.setBackground(drawable);
+                if(alarmList.size() == 0){
+                    recyclerView.setVisibility(View.INVISIBLE);
+                    textView.setVisibility(View.VISIBLE);
+                }
                 Snackbar.make(recyclerView, "已经成功删除", Snackbar.LENGTH_LONG)
                         .setAction("撤销删除", new View.OnClickListener() {
                                     @Override
                                     public void onClick(View v) {
                                         alarmList.add(pos, tempAlarm);
                                         mAdapter.notifyItemInserted(pos);
-                                        ColorDrawable drawable = new ColorDrawable(0x00000000);
-                                        if (0 == pos) recyclerView.setBackground(drawable);
+                                        recyclerView.setVisibility(View.VISIBLE);
+                                        textView.setVisibility(View.INVISIBLE);
                                     }
                                 }
 
@@ -129,6 +140,30 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private Bitmap drawTextAtBitmap(){
+        WindowManager windowManager = getWindowManager();
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        float screenWidth = metrics.widthPixels;
+        float screenHeight = metrics.heightPixels;
 
+        Bitmap newbit = Bitmap.createBitmap((int)screenWidth, (int)screenHeight, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(newbit);
+
+        canvas.drawColor(Color.WHITE);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(getResources().getColor(R.color.colorGray));
+        paint.setStyle(Paint.Style.FILL);
+        paint.setStrokeWidth(1);
+        paint.setTextSize(60);
+        canvas.drawText("目前没有闹钟，请添加.", 200.0f, 400.0f, paint);
+
+        canvas.save(Canvas.ALL_SAVE_FLAG);
+        canvas.restore();
+        return  newbit;
+    }
 
 }
---
+++ app/src/main/java/com/example/han/myalarmclock/RecyvclerViewEmptyBgView.java
@@ -0,0 +1,30 @@
+package com.example.han.myalarmclock;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+/**
+ * Created by Han on 2016/5/10.
+ */
+public class RecyvclerViewEmptyBgView extends View {
+    public RecyvclerViewEmptyBgView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(Color.WHITE);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(getResources().getColor(R.color.colorGray));
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(4);
+        paint.setTextSize(48);
+        paint.setShader(null);
+        canvas.drawText("Hello",1.0f,1.0f,paint);
+    }
+}
